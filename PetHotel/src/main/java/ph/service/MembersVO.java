cf011b4bfb79feb7e0c9aa6937f3535e907ed9bd
13 <13@DESKTOP-QKIEGPU>
2022-09-08 20:33:44 +0900

    test

--- PetHotel/src/main/java/ph/service/MembersVO.java
+++ PetHotel/src/main/java/ph/service/MembersVO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class MembersVO {	
+public class MembersVO {
 	private String memberId;
 	private String memberPassword;
 	private String memberCheckPassword;
